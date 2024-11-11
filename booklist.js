1618d21e4ca6a3f71b110a91edb73c50e7b987dd
Elchonon <ekn400@gmail.com>
2024-11-11 13:52:53 -0500

    drag drop added

--- booklist.js
+++ booklist.js
@@ -10,6 +10,9 @@ class Book {
         this.notes = notes
         this.apiId = apiId
     }
+    updateImage(url){
+        this.image = url
+    }
 }
 
 ///////////////////////////////////////////////////////
@@ -18,6 +21,7 @@ class Book {
 
 const bookMenu = document.querySelector("div#all-books")
 const searchForm = document.querySelector("form#search")
+const addDropZone = document.querySelector("p#add-dropzone")
 const baseUrl = "https://openlibrary.org" //search.json?q=javascript&fields=*,availability&limit=1
 const coverImgUrl = "https://covers.openlibrary.org"
 const localUrl = "http://localhost:3000/books"
@@ -61,6 +65,26 @@ function postJSON(payloadObj){
 ///////////////////////////////////////////////////////
 // rendering book functions
 //////////////////////////////////////////////////////
+function dragstartHandler(ev){
+    ev.dataTransfer.effectAllowed = "move"; //copy, move, link
+    const bookObj = new Book(ev.target.dataset.title, ev.target.dataset.author, ev.target.dataset.image, ev.target.dataset.subject, "", ev.target.dataset.apiId)
+    ev.dataTransfer.setData("text", JSON.stringify(bookObj))
+}
+function dragoverHandler(ev){
+    ev.preventDefault()
+    ev.dataTransfer.dropEffect = "move"; //copy,move,link
+}
+function dropHandler(ev){
+    ev.preventDefault()
+    const bookString = ev.dataTransfer.getData("text")
+    const bookObj = JSON.parse(bookString)
+    postJSON(bookObj)
+    
+    const saveResponse = `${bookObj.title} added to curriculum`
+    addDropZone.innerText = saveResponse
+}
+
+
 
 const renderBook = (book) => {
     //console.log(book)
@@ -72,27 +96,45 @@ const renderBook = (book) => {
     const bookImg = document.createElement("img")
     const bookOverlay = document.createElement("div")
     const bookSubject = document.createElement("p")
+
+    const bookObj = new Book(
+        book.title, book.author_name[0], 
+        "",
+        Array.isArray(book.subject) ? book.subject[0] : book.subject,
+        "",
+        book.cover_i
+    )
+    //console.log(bookObj)
     
-    curriculumBtn.dataset.id = book.cover_i
-    bookListing.dataset.id = book.cover_i
+    bookListing.draggable = true //allow draggable feature
+    bookListing.addEventListener("dragstart",dragstartHandler) //allow draggable feature
     
+    curriculumBtn.dataset.apiId = bookObj.apiId
     curriculumBtn.innerText = "add book to curriculum"
+
     bookListing.classList.add("book-listing")
     bookInfo.classList.add("book-info")
     bookOverlay.classList.add("overlay")
-    bookTitle.innerText = book.title
-    bookAuthor.innerText = book.author_name[0]
-    bookSubject.innerText = Array.isArray(book.subject) ? book.subject[0] : book.subject 
+    bookTitle.innerText = bookObj.title
+    bookAuthor.innerText = bookObj.author
+    bookSubject.innerText = bookObj.description 
     
-    bookImg.alt = `${book.title} cover image`
-    bookImg.id = book.cover_i
-    bookImg.style.height = "150px"
-    bookImg.style.width = "120px"
+    bookListing.dataset.title = bookObj.title
+    bookListing.dataset.author = bookObj.author
+    bookListing.dataset.subject = bookObj.description
+    bookListing.dataset.apiId = bookObj.apiId
+    
+    bookImg.alt = `${bookObj.title} cover image`
+    bookImg.id = bookObj.apiId
+    bookImg.style.height = "150px" //TODO: move to CSS
+    bookImg.style.width = "120px" //TODO: move to CSS
 
-    getJSON(`${coverImgUrl}/b/id/${book.cover_i}.json`)
+    getJSON(`${coverImgUrl}/b/id/${bookObj.apiId}.json`)
     .then((data) => {
-        //console.log(data.source_url)
-        bookImg.src = data.source_url
+        const updatedUrl = data.source_url
+        bookImg.src = updatedUrl
+        bookObj.image = updatedUrl
+        bookListing.dataset.image = updatedUrl
     })
     
     bookInfo.append(bookTitle)
@@ -113,7 +155,7 @@ const renderBookList = (searchString) => {
         query = encodeURI(searchString)
         bookMenu.innerHTML = `
         <div class="book-listing">
-            <div class="book-info">                   
+            <div class="book-info">
                 <h2>Books Loading</h2>
                 <p>please wait...</p>
             </div>
@@ -135,16 +177,30 @@ const renderBookList = (searchString) => {
 //////////////////////////////////////////////////////
 
 const handleSearch = (e) => {
-    const searchString = e.target['nook-search'].value
+    const searchString = e.target['book-search'].value
     e.preventDefault()
     renderBookList(searchString)
 }
 
 const handleCurriculumBtn = (e) => {
-    const bookId = e.target.dataset.id
-    console.log(bookId)
+    const bookDiv = e.currentTarget.parentNode
+    const bookObj = new Book(
+        bookDiv.getAttribute("data-title"), 
+        bookDiv.getAttribute("data-author"),
+        bookDiv.getAttribute("data-image"),
+        bookDiv.getAttribute("data-subject"),
+        "",
+        bookDiv.getAttribute("data-api-id")
+    )
+    console.log(bookObj)
+    console.log(`${bookObj.title} saved to curriculum`)
+    postJSON(bookObj)
     
-    const bookNodeList = document.querySelectorAll("div+"+'[data-id="'+bookId+'"]');
+    /*
+    const bookId = e.target.dataset.apiId
+    
+    //const bookNodeList = document.querySelectorAll("div+"+'[data-id="'+bookId+'"]'); //first book not being found
+    const bookNodeList = document.querySelectorAll('[data-id="'+bookId+'"]');
     const bookNode = bookNodeList[0]
     if(bookNode){
         const bookDiv = bookNode.childNodes[0]
@@ -161,6 +217,7 @@ const handleCurriculumBtn = (e) => {
     } else {
         console.error("book node not found")
     }
+    */
 }
 
 
@@ -168,6 +225,9 @@ const handleCurriculumBtn = (e) => {
 ///////////////////////////////////////////////////////
 // global declarations
 //////////////////////////////////////////////////////
+
+addDropZone.addEventListener('drop',dropHandler)
+addDropZone.addEventListener('dragover',dragoverHandler)
 
 searchForm.addEventListener('submit',handleSearch)
 
